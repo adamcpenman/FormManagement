f004ae7b827bbb06da68967cf1dcef7f3e0c1f5a
Adam <50333588+adamcpenman@users.noreply.github.com>
2019-09-09 18:55:02 -0500

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import Create from "./Create";
+
+function App() {
+  const [notes, setNotes] = useState([
+    {
+      title: "A Tale of Two Cities",
+      body:
+        "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair."
+    }
+  ]);
+
+  return (
+    <div className="App">
+      <h1>My Notes</h1>
+
+      <Create />
+
+      {notes.map((note, index) => (
+        <div className="note" key={index}>
+          <h2>{note.title}</h2>
+          <p>{note.body}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Create.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Create = props => {
+  return (
+    <form>
+      <input type="text" />
+      <button>Submit</button>
+    </form>
+  );
+};
+
+export default Create;
+
+// <form method="post" action="http://lambdaschool.com">
+{
+  /* <label for="textField">Some vlaue</label> */
+}
+{
+  /* <select>
+      <option></option>
+      <option>one</option>
+      <option>two</option>
+    </select>
+    <textarea></textarea> */
+}
