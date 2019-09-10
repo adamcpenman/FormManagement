1dddea4d1d4a32baf7a5bbb1b04952a31d77b344
Adam <50333588+adamcpenman@users.noreply.github.com>
2019-09-09 19:53:06 -0500

    event.target.name
    event.target.name

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,24 +1,42 @@
 import React, { useState } from "react";
 
 const Create = props => {
-  const [title, setTitle] = useState("");
-  const [body, setBody] = useState("");
-  const handleTextChange = event => {
-    setTitle(event.target.value);
+  // const [title, setTitle] = useState("");
+  // const [body, setBody] = useState("");
+  const [newNote, setNewNote] = useState({
+    title: "",
+    body: ""
+  });
+
+  const handleChange = event => {
+    setNewNote({ ...newNote, [event.target.name]: event.target.value });
   };
 
-  const handleBodyChange = event => {
-    setBody(event.target.value);
-  };
+  // const handleTitleChange = event => {
+  //   // setTitle(event.target.value);
+  //   const key = "title";
+  //   setNewNote( { ...newNote,
+  //     [event.target.name]: event.target.value });
+  // };
+
+  // const handleBodyChange = event => {
+  //   // setBody(event.target.value);
+  //   setNewNote({ ...newNote, body: event.target.value });
+  // };
 
   const handleSubmit = event => {
     event.preventDefault();
-    console.log("form submitted");
+    console.log(newNote);
   };
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" onChange={handleTextChange} />
-      <textarea onChange={handleBodyChange} />
+      <input
+        type="text"
+        name="title"
+        placeholder="Title"
+        onChange={handleChange}
+      />
+      <textarea name="body" onChange={handleChange} />
       <button type="submit">Submit</button>
     </form>
   );
